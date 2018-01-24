29645dbaac5de64f6aa79509d87f80100c1ece9f
zhangmk <51670196@qq.com>
2018-01-24 14:17:29 +0800

    add feature-eng-desc

--- Feature/feature-eng-desc.py
+++ Feature/feature-eng-desc.py
@@ -1,8 +1,6 @@
 import sys
 import numpy as np
 import pandas as pd
-import matplotlib as mlp
-import matplotlib.pyplot as plt
 sys.path.append("..")
 import DB.db_connects as dbconn
 
@@ -17,11 +15,8 @@ train_cursor = train_collection.find({})
 df_test = pd.DataFrame(list(test_cursor))
 df_train = pd.DataFrame(list(train_cursor))
 
+df_train.describe().to_csv("~/data/desc-training.csv")
+df_test.describe().to_csv("~/data/desc-testing.csv")
 
-#data = pd.read_csv('../data/cs-training.csv')
-#test = pd.read_csv()
+# a = np.log(data.RevolvingUtilizationOfUnsecuredLines+1)
 
-#"feature-eng description"
-#data.describe().to_csv("desc-training.csv")
-
-#a = np.log(data.RevolvingUtilizationOfUnsecuredLines+1)
