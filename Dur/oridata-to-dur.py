5ff36d995f7afe89d0e81e3c909bec65f60fd1e2
zhangmk <51670196@qq.com>
2018-01-26 11:08:19 +0800

    h5

--- Dur/oridata-to-dur.py
+++ Dur/oridata-to-dur.py
@@ -15,6 +15,8 @@ df_test = pd.DataFrame(list(test_cursor))
 df_train = pd.DataFrame(list(train_cursor))
 
 h5filename = '/home/python/data/dur/store.h5'
-# store = pd.HDFStore(h5filename)
-df_test.to_hdf(h5filename,'test',mode='w', table=True )
-df_train.to_hdf(h5filename,'train',mode = 'w', table = True)
+store = pd.HDFStore(h5filename)
+store.put('test', df_test, format='table')
+store.put('train', df_train, format='train')
+# df_test.to_hdf(h5filename,'test',mode='w', table=True )
+# df_train.to_hdf(h5filename,'train',mode = 'w', table = True)
