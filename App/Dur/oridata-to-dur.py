39f31b67a1741f9c919fbbfcf171a5c9e7f80262
zhangmk <51670196@qq.com>
2018-02-01 10:27:45 +0800

    use config

--- App/Dur/oridata-to-dur.py
+++ App/Dur/oridata-to-dur.py
@@ -1,22 +1,16 @@
 import sys
 import os
+sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
 import numpy as np
 import pandas as pd
 import configparser
-config = configparser.ConfigParser()
-config.readfp(open(sys.path[0] + '/../Conf/main.conf'))
-CSV_DATA_PATH = config.get('data_source', 'csv_data_path')
-DATA_FROM = config.get('data_source', 'ori_data_from')
-CSV_FILE_TEST = config.get('data_source', 'csv_file_test')
-CSV_FILE_TRAIN = config.get('data_source', 'csv_file_train')
-H5FILENAME = config.get('data_source', 'h5filename')
-WORK_PATH = config.get('data_source', 'work_path')
+from Conf.loadconf import *
+import DB.db_connects as dbconn
+
 df_test = pd.DataFrame()
 df_train = pd.DataFrame()
 
 if DATA_FROM == 'MongoDB':
-    sys.path.append(WORK_PATH)
-    import DB.db_connects as dbconn
     client = dbconn.microrulemongoclient
     db = client['rule']
     test_collection = db['test_model_data']
--- App/Feature/feature-eng-desc.py
+++ App/Feature/feature-eng-desc.py
@@ -1,17 +1,9 @@
 import sys
 import os
+sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
 import numpy as np
 import pandas as pd
-import configparser
-config = configparser.ConfigParser()
-config.readfp(open(sys.path[0] + '/../Conf/main.conf'))
-CSV_DATA_PATH = config.get('data_source', 'csv_data_path')
-DATA_FROM = config.get('data_source', 'ori_data_from')
-CSV_FILE_TEST = config.get('data_source', 'csv_file_test')
-CSV_FILE_TRAIN = config.get('data_source', 'csv_file_train')
-H5FILENAME = config.get('data_source', 'h5filename')
-WORK_PATH = config.get('data_source', 'work_path')
-
+from Conf.loadconf import *
 
 store = pd.HDFStore(os.path.join(CSV_DATA_PATH, H5FILENAME))
 test = store['test']
